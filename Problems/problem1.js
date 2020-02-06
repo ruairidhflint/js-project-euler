6b9133ae987e83b8f59e394cc0bbbf68e078aa6e
Rory Flint <roryflintphoto@gmail.com>
2020-02-06 12:34:59 +0000

    Solve problem 3, start problem 4

--- Problems/problem1.js
+++ Problems/problem1.js
@@ -18,4 +18,4 @@ Find the sum of all the multiples of 3 or 5 below 1000.
    }
  }
 
- console.log(answer + 1);
+ console.log(answer);
--- Problems/problem3.js
+++ Problems/problem3.js
@@ -19,16 +19,39 @@ What is the largest prime factor of the number 600851475143?
 function primeFinder(n) {
   let isPrime = true;
   if (n > 0 && n <= 2) {
-      return isPrime;
+    return isPrime;
   }
 
-  for (let i = 2; i <= Math.sqrt(n); i++){
-      if (n % i === 0){
-          isPrime = false;
-          break;
-      }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      isPrime = false;
+      break;
+    }
   }
   return isPrime;
 }
 
-console.log(primeFinder(39430));
+function largestPrimeFactor(number) {
+  //check number itself is not a prime
+  if (primeFinder(number)) {
+    return number;
+  }
+  // Initiate array for factors to be pushed to
+  let factors = [];
+  //take square root of number, loop from 2 upwards checking if any number 
+  //is divisible, if so, push it and it's counterpart to array
+  for (let i = 2; i <= Math.sqrt(number) / 2; i++) {
+    if (number % i === 0) {
+      factors.push(i);
+      factors.push(number / i);
+    }
+  }
+  //filter through array, checking if any are primes
+  const primeFactors = factors.filter(x => {
+    return primeFinder(x);
+  });
+  //return largest value
+  return primeFactors[primeFactors.length - 1];
+}
+
+console.log(largestPrimeFactor(600851475143));
